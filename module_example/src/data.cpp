1b721fd7be4601aade0a47a1c3f738702f446cf2
shengxiaoyi1993 <shengxiaoyi2014@icloud.com>
2020-04-01 10:00:18 +0800

    modify template project

--- module_example/src/data.cpp
+++
@@ -1,7 +0,0 @@
-#include"data.h"
-
-using namespace std;
-
-Data::Data(int zz){
-  cout<<"zz: "<<zz<<endl;
-}
--- module_example/src/main.cpp
+++ module_example/src/main.cpp
@@ -1,10 +1,9 @@
-#include"data.h"
 #include<iostream>
 
 #define SECONDS_PER_YEAR  60*60*24*365UL
 
  int main() {
-   std::cout<<SECONDS_PER_YEAR<<endl;
+   std::cout<<SECONDS_PER_YEAR<<std::endl;
 
   return 0;
 }

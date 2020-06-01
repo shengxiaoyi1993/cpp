c41d55d44b771f48c7bf623c5ecb8e7d4a812739
sxy <sxy@localhost.localdomain>
2020-06-01 19:35:24 +0800

    change in broadcast

--- broadcast/doc/manuals/manuals.md
+++ broadcast/doc/manuals/manuals.md
@@ -1,3 +1,4 @@
 # manuals
 >用于说明该文档的使用
 - 详细方法见`build/build.md`
+
--- broadcast/src/src_client/client_multibroadcast_linux.cpp
+++ broadcast/src/src_client/client_multibroadcast_linux.cpp
@@ -15,62 +15,63 @@
 using namespace std;
 int main()
 {
-  int sock;
-  struct sockaddr_in local_addr;
-  int err=-1;
-  sock=socket(AF_INET,SOCK_DGRAM,0);
-  if(sock==-1){
-     cout<<"sock error"<<endl;
-      return -1;
-   }
-   /*初始化地址*/
-   local_addr.sin_family=AF_INET;
-   local_addr.sin_addr.s_addr=htonl(INADDR_ANY);
-   local_addr.sin_port=htons(MCAST_PORT);
-   /*绑定socket*/
-   err=bind(sock,(struct sockaddr*)&local_addr,sizeof(local_addr));
-   if(err<0){
-     cout<<"bind error"<<endl;
-     return -2;
-   }
-   /*设置回环许可*/
-   int loop=1;
-   err=setsockopt(sock,IPPROTO_IP,IP_MULTICAST_LOOP,&loop,sizeof(loop));
-   if(err<0){
-     cout<<"set sock error"<<endl;
-     return -3;
-   }
-   struct ip_mreq mreq;/*加入广播组*/
-   mreq.imr_multiaddr.s_addr=inet_addr(MCAST_ADDR);//广播地址
-   mreq.imr_interface.s_addr=htonl(INADDR_ANY); //网络接口为默认
-   /*将本机加入多播组*/
-   err=setsockopt(sock,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq));
-   if(err<0){
-     cout<<"set sock error"<<endl;
-       return -4;
-   }
-
-  int times=0;
-  socklen_t addr_len=0;
-  char buff[BUFF_SIZE];
-  int n=0;
-  /*循环接受广播组的消息，5次后退出*/
-  for(times=0;;times++)
-  {
-    addr_len=sizeof(local_addr);
-    memset(buff,0,BUFF_SIZE);
-    n=recvfrom(sock,buff,BUFF_SIZE,0,(struct sockaddr*)&local_addr,&addr_len);
-    if(n==-1){
-      cout<<"recv error"<<endl;
-      return -5;
+    int sock;
+    struct sockaddr_in local_addr;
+    int err=-1;
+    sock=socket(AF_INET,SOCK_DGRAM,0);
+    if(sock==-1){
+        cout<<"sock error"<<endl;
+        return -1;
     }
-    /*打印信息*/
-    printf("RECV %dst message from server : %s\n",times,buff);
-    sleep(MCAST_INTERVAL);
-  }
+    /*初始化地址*/
+    local_addr.sin_family=AF_INET;
+    local_addr.sin_addr.s_addr=htonl(INADDR_ANY);
+    local_addr.sin_port=htons(MCAST_PORT);
+    /*绑定socket*/
+    err=bind(sock,(struct sockaddr*)&local_addr,sizeof(local_addr));
+    if(err<0){
+        cout<<"bind error"<<endl;
+        return -2;
+    }
+    /*设置回环许可*/
+    int loop=1;
+    err=setsockopt(sock,IPPROTO_IP,IP_MULTICAST_LOOP,&loop,sizeof(loop));
+    if(err<0){
+        cout<<"set sock error"<<endl;
+        return -3;
+    }
+    struct ip_mreq mreq;/*加入广播组*/
+    mreq.imr_multiaddr.s_addr=inet_addr(MCAST_ADDR);//广播地址
+    mreq.imr_interface.s_addr=htonl(INADDR_ANY); //网络接口为默认
+    /*将本机加入多播组*/
+    err=setsockopt(sock,IPPROTO_IP,IP_ADD_MEMBERSHIP,&mreq,sizeof(mreq));
+    if(err<0){
+        cout<<"set sock error"<<endl;
+        return -4;
+    }
 
-   /*退出多播组*/
-  err=setsockopt(sock,IPPROTO_IP,IP_DROP_MEMBERSHIP,&mreq,sizeof(mreq));
-  close(sock);
-  return 0;
+    int times=0;
+    socklen_t addr_len=0;
+    char buff[BUFF_SIZE];
+    int n=0;
+    /*循环接受广播组的消息，5次后退出*/
+    for(times=0;;times++)
+    {
+        cout<<"times:"<<times<<endl;
+        addr_len=sizeof(local_addr);
+        memset(buff,0,BUFF_SIZE);
+        n=recvfrom(sock,buff,BUFF_SIZE,0,(struct sockaddr*)&local_addr,&addr_len);
+        if(n==-1){
+            cout<<"recv error"<<endl;
+            return -5;
+        }
+        /*打印信息*/
+        printf("RECV %dst message from server : %s\n",times,buff);
+        sleep(MCAST_INTERVAL);
+    }
+
+    /*退出多播组*/
+    err=setsockopt(sock,IPPROTO_IP,IP_DROP_MEMBERSHIP,&mreq,sizeof(mreq));
+    close(sock);
+    return 0;
 }

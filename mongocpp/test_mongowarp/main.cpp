7bb6655a4805c5b74a168d0c9177d9cfb4a26951
sxy <sxy@localhost.localdomain>
2020-06-02 08:00:40 -0400

    mongo change

--- mongocpp/test_mongowarp/main.cpp
+++ mongocpp/test_mongowarp/main.cpp
@@ -79,9 +79,8 @@ void api_test(){
 }
 
 
-void mongo_insertdata(uint v_quentity){
+void mongo_insertdata(uint v_quentity,string v_tablename){
     mongo::Init();
-    char ptable[]="tb_search_450k";
 
     for(uint i=0;i<v_quentity;i++){
         string tmp=getSha512ForUint(i);
@@ -89,14 +88,11 @@ void mongo_insertdata(uint v_quentity){
                 +tmp+"\",\"description\":\"hhh\""
                      "}";
 
-        char* pid_insert= Add((char*)query_insert.c_str(),ptable);
-        //        cout<<"pid_insert:"<<query_insert<<endl;
+        char* pid_insert= Add((char*)query_insert.c_str(),(char*)v_tablename.c_str());
 
     }
 
-    //    char pdoc0[]="{\"name\":\"sxy\"}";
-    //    char* pid_insert= Add(pdoc0,ptable);
-    //    cout<<"pid_insert:"<<pid_insert<<endl;
+
 
     mongo::Cleanup();
 
@@ -104,26 +100,24 @@ void mongo_insertdata(uint v_quentity){
 
 
 
-void mongo_searchdata(uint v_repeat,uint v_quentity){
+void mongo_searchdata(uint v_repeat,uint v_quentity,string v_tablename){
 
     mongo::Init();
-    char ptable[]="tb_search_450k";
+//    char ptable[]="tb_search_450k";
     vector<string> list= getNumFrom(10000,v_quentity);
     time_t record_time = time(0);
 
 
     for(uint i=0;i<v_repeat;i++){
         for(uint j=0;j<v_quentity;j++){
-//            stringstream  logcout("/home/sxy/Github/cpp/mongocpp/build-test_mongowarp-Desktop_Qt_5_9_7_GCC_64bit-Debug/mongo_search.log");
 
             string query_tmp="{\"condi\":\""+list[j]+
                     "\"}";
 
-//            logcout<<"query_tmp:"<<query_tmp<<endl;
-            cout<<"query_tmp:"<<query_tmp<<endl;
+//            cout<<"query_tmp:"<<query_tmp<<endl;
 
 
-            char* result=FindMany((char*)query_tmp.c_str(),0,0,ptable);
+            char* result=FindMany((char*)query_tmp.c_str(),0,0,(char*)v_tablename.c_str());
             cout<<"result:"<<result<<endl;
             time_t current_time = time(0);
             cout<<"ONE search:"<<current_time-record_time<<endl;
@@ -139,6 +133,69 @@ int main(int argc, char *argv[])
 {
     //    api_test();
 //        mongo_insertdata(4361168);
-    mongo_searchdata(1000,1000);
+//    mongo_searchdata(1000,1000,);
+
+    vector<uint> list_total;
+    vector<string> list_table;
+    list_total.push_back(10000);
+    list_table.push_back("tb_10k");
+
+    list_total.push_back(100000);
+    list_table.push_back("tb_100k");
+
+    list_total.push_back(500000);
+    list_table.push_back("tb_500k");
+
+    list_total.push_back(1000000);
+    list_table.push_back("tb_1m");
+
+    list_total.push_back(5000000);
+    list_table.push_back("tb_5m");
+
+//    list_total.push_back(10000000);
+//    list_table.push_back("tb_10m");
+
+//    list_total.push_back(50000000);
+//    list_table.push_back("tb_50m");
+
+//    list_total.push_back(100000000);
+//    list_table.push_back("tb_100m");
+
+
+
+
+    //   insert
+    for(uint ntab=0;ntab<list_table.size();ntab++){
+//        int createflag = mysql_createtable(list_table[ntab]);
+//        if(createflag != 0 ){
+//            continue;
+//        }
+//        mysql_cleantable(list_table[ntab]);
+        int flag_insert=mysql_insertdata(list_total[ntab],list_table[ntab]);
+        if(flag_insert == 0){
+            cout<<list_table[ntab]<<" create succeed"<<endl;
+        }
+        else{
+            cout<<list_table[ntab]<<" create failed"<<endl;
+
+        }
+
+    }
+
+
+//    //search
+//    uint times = 1;
+//    uint quentity=1000;
+//    cout<<"| total |"<<" times |"<<" quentity |"<<" cost |"<<endl;
+//    for(uint ntab=0;ntab<list_table.size();ntab++){
+//        int time= mysql_search(times,quentity,list_total[ntab],list_table[ntab]);
+//        cout<<"| "<<list_total[ntab]<<" | "<<times<<" | "<<quentity<<" | "<<time<<" | "<<endl;
+//        time= mysql_search(times,quentity,list_total[ntab],list_table[ntab]);
+//        cout<<"| "<<list_total[ntab]<<" | "<<times<<" | "<<quentity<<" | "<<time<<" | "<<endl;
+//        time= mysql_search(times,quentity,list_total[ntab],list_table[ntab]);
+//        cout<<"| "<<list_total[ntab]<<" | "<<times<<" | "<<quentity<<" | "<<time<<" | "<<endl;
+//        cout<<endl;
+//    }
+
     return 0;
 }

d72566862875c5f19a92d5fa3df5fa7d48b507df
sxy <sxy@localhost.localdomain>
2020-06-02 16:51:37 +0800

    change

--- mysql/src/basic.cpp
+++ mysql/src/basic.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include<stdlib.h>
 #include <sstream>
+#include <sys/time.h>
 
 using namespace std;
 
@@ -10,18 +11,21 @@ using namespace std;
 
 
 uint getRandom(uint v_low,uint v_high){
-        srand(time(nullptr));
-        uint interval=v_high-v_low;
-        uint num=random()%interval+v_low;
-        return num;
+    struct timeval tv;
+    struct timezone tz;
+    gettimeofday(&tv,&tz);
+    srand(tv.tv_sec*1000000 +tv.tv_usec);
+    uint interval=v_high-v_low;
+    uint num=random()%interval+v_low;
+    return num;
 }
 
 std::string integerToString(uint num){
-  stringstream ss;
-  ss.clear();
-  ss.str("");
-  string tmps;
-  ss<<num;
-  ss>>tmps;
-  return tmps;
+    stringstream ss;
+    ss.clear();
+    ss.str("");
+    string tmps;
+    ss<<num;
+    ss>>tmps;
+    return tmps;
 }
--- mysql/src/mysql_query.cpp
+++ mysql/src/mysql_query.cpp
@@ -3,12 +3,53 @@
 #include <mysql/mysql.h>
 #include "sha512.h"
 #include "basic.h"
+#include <sys/time.h>
+
 
 using namespace std;
 
 
 
-void mysql_insertdata(uint v_num){
+const string name_db="test_db";
+const string pw_db="123456";
+const string user_db="root";
+const string ip_db="127.0.0.1";
+
+
+
+
+int mysql_createtable(string v_tablename){
+    MYSQL conn;
+    int res;
+    MYSQL_RES *result = NULL;
+    MYSQL_FIELD *field = NULL;
+    mysql_init(&conn);
+    if(mysql_real_connect(&conn,"127.0.0.1","root","123456","test_db",0,NULL,CLIENT_FOUND_ROWS))
+    {
+
+        string query="CREATE TABLE IF NOT EXISTS "
+                +v_tablename+
+                "("
+                "id INT(11) UNSIGNED PRIMARY KEY,"
+                " condi VARCHAR(128),"
+                "description VARCHAR(10))";
+
+        res = mysql_query(&conn,query.c_str());
+        if(res == 0){
+            cout<<"succeed to create"<<endl;
+        }
+        else{
+            cout<<"failed to create"<<endl;
+        }
+        return res;
+
+    }
+    return -1;
+
+}
+
+
+int mysql_insertdata(uint v_num,string v_tablename){
     MYSQL conn;
     int res;
     MYSQL_RES *result = NULL;
@@ -17,142 +58,185 @@ void mysql_insertdata(uint v_num){
 
     uint inputnum=v_num;
 
-    //222555yq为数据库的进入密码，yg_sql为数据库名
-    if(mysql_real_connect(&conn,"localhost","root","123456","test_db",0,NULL,CLIENT_FOUND_ROWS))
+    if(mysql_real_connect(&conn,"127.0.0.1","root","123456","test_db",0,NULL,CLIENT_FOUND_ROWS))
     {
-        cout << "connect success!" << endl;
-
-
-        //         res = mysql_query(&conn,"create database tb_search");
-
-        //         if(res == 0){
-        //             cout<<"succeed to create database"<<endl;
-        //         }
-        //         else{
-        //             cout<<"failed to create database"<<endl;
-        //         }
-
-        //         res = mysql_query(&conn,"SHOW DATABASES");
-        ////         res = mysql_query(&conn,"USE tb_search");
-
-        //         if(res == 0){
-        //             cout<<"succeed to create database"<<endl;
-        //         }
-        //          result = mysql_store_result(&conn);
-
-
-        res = mysql_query(&conn,"CREATE TABLE tb_search("
-                                "id INT(11) UNSIGNED PRIMARY KEY,"
-                                " condi VARCHAR(128),"
-                                "description VARCHAR(10))");
-        if(res == 0){
-            cout<<"succeed to create TABLE"<<endl;
-        }
-        else{
-            cout<<"failed to create TABLE"<<endl;
-        }
-
         for(uint i=0;i<inputnum;i++){
-
             string codition = getSha512ForUint(i);
-            string insert_query="INSERT INTO tb_search VALUES("+integerToString(i)
+            string insert_query="INSERT INTO "
+                    +v_tablename+
+                    " VALUES("+integerToString(i)
                     +",'"+codition+"','hhh'"+")";
             mysql_query(&conn,insert_query.c_str());
-
-            MYSQL_RES *res_query;
-            MYSQL_ROW row;
-            res_query = mysql_store_result(&conn);
-
-            if (NULL == res_query)
-            {
-                cout << "mysql_store_result failure！" << endl;
-            }
-
-            // 重复读取行，并输出第一个字段的值，直到row为NULL
-
-            while (row = mysql_fetch_row(res_query))
-            {
-                cout << row[0 ] << endl;
-            }
-
-            // 释放结果集
-            mysql_free_result(res_query);
         }
-
-
         mysql_close(&conn);
+        return 0;
     }
     else{
-        cout<<"Failed to connect!"<<endl;
+        return -1;
     }
 }
 
 
-void mysql_search(uint v_repeat,uint v_quentity){
+int mysql_cleantable(string v_tablename){
+
+    MYSQL conn;
+    mysql_init(&conn);
+
+    if(mysql_real_connect(&conn,"127.0.0.1","root","123456","test_db",0,NULL,CLIENT_FOUND_ROWS))
+    {
+        string clean_query="TRUNCATE "+v_tablename;
+        int flag_query=  mysql_query(&conn,clean_query.c_str());
+        return flag_query;
+    }
+    else {
+        return -1;
+    }
+
+}
+
+int  mysql_search(uint v_repeat,uint v_quentity,uint v_total,string v_tablename){
+
+    struct timeval tv_record;
+    struct timezone tz_record;
+
+    struct timeval tv_current;
+    struct timezone tz_current;
+
 
 
     MYSQL conn;
-    int res;
-    MYSQL_RES *result = NULL;
-    MYSQL_FIELD *field = NULL;
     mysql_init(&conn);
+    vector<string> list= getNumFrom(v_total,v_quentity);
 
-    vector<string> list= getNumFrom(10000,v_quentity);
-    //    cout<<"list[0]:"<<list[0]<<endl;
 
-    //222555yq为数据库的进入密码，yg_sql为数据库名
     if(mysql_real_connect(&conn,"127.0.0.1","root","123456","test_db",0,NULL,CLIENT_FOUND_ROWS))
     {
-        cout << "connect success!" << endl;
 
-        time_t record_time = time(0);
+        gettimeofday(&tv_record,&tz_record);
         for(uint i=0;i<v_repeat;i++){
             for(uint j=0;j<v_quentity;j++){
-                string insert_query="SELECT * FROM tb_search WHERE condi='"+list[j]+"'";
-                cout<<"insert_query:"<<insert_query<<endl;
+                string insert_query="SELECT * FROM "
+                        +v_tablename+
+                        " WHERE condi='"+list[j]+"'";
                 int flag_query=  mysql_query(&conn,insert_query.c_str());
-                cout<<"flag_query:"<<flag_query<<endl;
+
                 MYSQL_RES *res_query;
                 MYSQL_ROW row;
                 res_query = mysql_store_result(&conn);
 
                 if (NULL == res_query)
                 {
-                    cout << "mysql_store_result failure！" << endl;
-                    return;
+                    continue;
+                    //                    cout << "mysql_store_result failure！" << endl;
+                }
+                else{
+                    //                    unsigned int num_fields;
+                    //                    unsigned int i;
+                    //                    num_fields = mysql_num_fields(res_query);
+                    //                    while ((row = mysql_fetch_row(res_query)))
+                    //                    {
+                    //                        unsigned long *lengths;
+                    //                        lengths = mysql_fetch_lengths(res_query);
+                    //                        for(i = 0; i < num_fields; i++)
+                    //                        {
+                    //                            printf("[%.*s] ", (int) lengths[i], row[i] ? row[i] : "NULL");
+                    //                        }
+                    //                        printf("\n");
+                    //                    }
+
+                    // 释放结果集
+                    mysql_free_result(res_query);
                 }
 
-                time_t current_time = time(0);
-                cout<<"ONE search:"<<current_time-record_time<<endl;
 
-                // 重复读取行，并输出第一个字段的值，直到row为NULL
-
-                while (row = mysql_fetch_row(res_query))
-
-                {
-                    cout << row[0 ] << endl;
-                }
-
-                // 释放结果集
-                mysql_free_result(res_query);
             }
         }
 
-        time_t current_time = time(0);
-        cout<<"total_time:"<<current_time-record_time<<endl;
-        mysql_close(&conn);
+        //        time_t current_time = time(0);
+        //        cout<<"total_time:"<<current_time-record_time<<endl;
+        gettimeofday(&tv_current,&tz_current);
+        //        cout<<"total time:"<<(tv_current.tv_sec-tv_record.tv_sec)*1000000
+        //              +tv_current.tv_usec-tv_record.tv_usec<<endl;
 
+
+        mysql_close(&conn);
+        return (tv_current.tv_sec-tv_record.tv_sec)*1000000
+                +tv_current.tv_usec-tv_record.tv_usec;
     }
     else{
         cout << "Fail to connect!" << endl;
 
     }
+    return -1;
 }
+
 
 
 int main(int argc,char *argv[])
 {
-    mysql_search(1000,1000);
+    vector<uint> list_total;
+    vector<string> list_table;
+    list_total.push_back(10000);
+    list_table.push_back("tb_10k");
+
+    list_total.push_back(100000);
+    list_table.push_back("tb_100k");
+
+    list_total.push_back(500000);
+    list_table.push_back("tb_500k");
+
+    list_total.push_back(1000000);
+    list_table.push_back("tb_1m");
+
+    list_total.push_back(5000000);
+    list_table.push_back("tb_5m");
+
+//    list_total.push_back(10000000);
+//    list_table.push_back("tb_10m");
+
+//    list_total.push_back(50000000);
+//    list_table.push_back("tb_50m");
+
+//    list_total.push_back(100000000);
+//    list_table.push_back("tb_100m");
+
+
+
+
+    //   insert
+    for(uint ntab=0;ntab<list_table.size();ntab++){
+        int createflag = mysql_createtable(list_table[ntab]);
+        if(createflag != 0 ){
+            continue;
+        }
+        mysql_cleantable(list_table[ntab]);
+        int flag_insert=mysql_insertdata(list_total[ntab],list_table[ntab]);
+        if(flag_insert == 0){
+            cout<<list_table[ntab]<<" create succeed"<<endl;
+        }
+        else{
+            cout<<list_table[ntab]<<" create failed"<<endl;
+
+        }
+
+    }
+
+
+//    //search
+//    uint times = 1;
+//    uint quentity=1000;
+//    cout<<"| total |"<<" times |"<<" quentity |"<<" cost |"<<endl;
+//    for(uint ntab=0;ntab<list_table.size();ntab++){
+//        int time= mysql_search(times,quentity,list_total[ntab],list_table[ntab]);
+//        cout<<"| "<<list_total[ntab]<<" | "<<times<<" | "<<quentity<<" | "<<time<<" | "<<endl;
+//        time= mysql_search(times,quentity,list_total[ntab],list_table[ntab]);
+//        cout<<"| "<<list_total[ntab]<<" | "<<times<<" | "<<quentity<<" | "<<time<<" | "<<endl;
+//        time= mysql_search(times,quentity,list_total[ntab],list_table[ntab]);
+//        cout<<"| "<<list_total[ntab]<<" | "<<times<<" | "<<quentity<<" | "<<time<<" | "<<endl;
+//        cout<<endl;
+//    }
+
+
 
     return 0;
 }

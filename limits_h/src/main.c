7c5d159eaf92e3315398cb91d5be526f752b22e1
qc_da4e05189d814e28850771a1831dbc61 <shengxiaoyi2014@icloud.com>
2021-05-10 21:05:36 +0800

    up

---
+++ limits_h/src/main.c
@@ -0,0 +1,37 @@
+#include <limits.h>
+#include <stdio.h>
+
+/// 定义了整数类型的范围
+/// char
+/// unsigned char
+/// int
+/// unsigned int
+/// long
+/// unsigned long
+
+
+int main(int argc, char *argv[])
+{
+  printf("CHAR_BIT:%d\n",CHAR_BIT);
+  printf("CHAR_MIN:%d\n",CHAR_MIN);
+  printf("CHAR_MAX:%d\n",CHAR_MAX);
+
+  printf("INT_MIN:%d\n",INT_MIN);
+  printf("INT_MAX:%d\n",INT_MAX);
+
+  printf("UINT_MAX:%d\n",UINT_MAX);
+
+  printf("SCHAR_MIN:%d\n",SCHAR_MIN);
+  printf("SCHAR_MAX:%d\n",SCHAR_MAX);
+
+  printf("UCHAR_MAX:%d\n",UCHAR_MAX);
+///...
+
+
+
+
+
+
+  return 0;
+}
+
